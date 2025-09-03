48c142603f2f30cb3f0596d443f30e69d329ccf3
6gdfg <115078566+6gdfg@users.noreply.github.com>
2025-09-03 20:54:56 +0800

    Update config.js

--- themes/simple/config.js
+++ themes/simple/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
 
-  SIMPLE_LOGO_IMG: '/Logo.webp',
+  SIMPLE_LOGO_IMG: 'https://imgbed.6gdfg.tech/file/1756903281338_8076acd083c2efe70aeda6b756d64d5.jpg',
   SIMPLE_TOP_BAR: true, // 显示顶栏
   SIMPLE_TOP_BAR_CONTENT: process.env.NEXT_PUBLIC_THEME_SIMPLE_TOP_TIPS || '',
   SIMPLE_LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>爱过生日的集体<br/>/团结 友善 进取</div>',
